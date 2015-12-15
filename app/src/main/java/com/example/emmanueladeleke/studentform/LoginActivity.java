58f381df583f12a70c59326efd879dbad81fab9a
Emmanuel Adeleke <emi_emi@hotmail.co.uk>
2015-12-15 10:27:56 +0000

    Authenticate user to database

--- app/src/main/java/com/example/emmanueladeleke/studentform/LoginActivity.java
+++ app/src/main/java/com/example/emmanueladeleke/studentform/LoginActivity.java
@@ -3,14 +3,26 @@ package com.example.emmanueladeleke.studentform;
 import android.app.ProgressDialog;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,6 +36,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+//
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//        StrictMode.setThreadPolicy(policy);
 
         tvTitle = (TextView) findViewById(R.id.tvTitle);
 
@@ -34,6 +49,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etPassword = (EditText) findViewById(R.id.etPassword);
         bConnect = (Button) findViewById(R.id.bConnect);
 
+
+
         bConnect.setOnClickListener(this);
     }
 
@@ -41,34 +58,79 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         if (v.getId() == R.id.bConnect) {
             LoginTask task = new LoginTask();
-            task.execute();
+            task.execute(etUsername.getText().toString(), etPassword.getText().toString());
+            task.on
 
         }
     }
 
-    private class LoginTask extends AsyncTask<String, Void, Void> {
+    private class LoginTask extends AsyncTask<String, String, String> {
 
         ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
-
+        boolean result = false;
         @Override
         protected void onPreExecute() {
             dialog.setMessage("Authenticating user...");
             dialog.show();
+
             boolean result = false;
         }
 
         @Override
-        protected Void doInBackground(String... params) {
+        protected String doInBackground(String... params) {
+
+            String username = params[0];
+            String password = params[1];
+
+            Log.d("Username", username);
+            Log.d("Password", password);
+
+            String strJson;
+            URL url = null;
+            BufferedReader in = null;
             try {
-                Thread.sleep(9000);
-            } catch (InterruptedException e) {
+                url = new URL("http://emmanueladeleke.ddns.net:3000/otm/lecturer?query={\"username\":\"" + username + "\",\"password\":\"" + password + "\"}");
+                in = new BufferedReader(new InputStreamReader(url.openStream()));
+            }
+            catch(Exception e) {
                 e.printStackTrace();
             }
-            return null;
+
+            String inputLine;
+            StringBuilder builder = new StringBuilder();
+            try {
+                while ((inputLine = in.readLine()) != null)
+                    builder.append(inputLine);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                in.close();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+            strJson = builder.toString().replaceAll("\\s+","");
+            System.out.println();
+
+            if (strJson.equals("[]")) {
+                Log.d("hey", strJson);
+            }
+            else {
+                //createFile(strJson);
+                Log.d("hey", strJson);
+            }
+
+            //AuthUser authUser = new AuthUser(username, password);
+           // authUser.authenticate();
+            //Log.d("test!", authUser.authenticate() + "");
+            //Log.d("username", username);
+        return strJson;
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
+        protected void onPostExecute(String s) {
             dialog.dismiss();
         }
     }
