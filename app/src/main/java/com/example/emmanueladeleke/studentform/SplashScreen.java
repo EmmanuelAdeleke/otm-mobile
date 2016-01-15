860b9d81bbded5b10ce59ad744f3665bdd263829
Emmanuel Adeleke <emi_emi@hotmail.co.uk>
2016-01-15 00:58:59 +0000

    Implement SplashScreen activity and intent

---
+++ app/src/main/java/com/example/emmanueladeleke/studentform/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.emmanueladeleke.studentform;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView tvTitle;
+    Typeface titleFont;
+    int SPLASH_DISPLAY_LENGTH = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Custom font
+        tvTitle = (TextView) findViewById(R.id.otmtitle);
+        titleFont = Typeface.createFromAsset(getAssets(), "fonts/titleFont.ttf");
+        tvTitle.setTypeface(titleFont);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
